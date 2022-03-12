c0fd09a40ddac0d24145c5b0d4b3c03fbd8e455d
kali <rafi.khudayar@gmail.com>
2022-03-12 20:53:15 +0300

    Blue Canary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import {Routes} from "react-router";
+import Sign_Up from "./pages/Sign_Up";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Login />
+      </div>
+
   );
 }
 
